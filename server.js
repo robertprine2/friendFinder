749e59034494b2ad75c8e970e777ddd1c9294b19
Robert Prine <robertprine2@yahoo.com>
2016-06-25 18:51:03 -0400

    added fairly basic html pages with some css and server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+// npm packages
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// expess variable and port variable
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+
+// listens for different routes defined in api-routes.js and html-routes.js
+
+require('./routing/api-routes.js')(app);
+require('./routing/html-routes.js')(app);
+
+app.listen(PORT, function() {
+	console.log("App listening on PORT: " + PORT);
+});
